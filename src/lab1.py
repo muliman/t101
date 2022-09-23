675241ad0d074f7092323baf866bf73d9383014a
muliman <muliman.ru>
2022-09-23 22:48:37 +0300

    add other docstrings

--- src/lab1.py
+++ src/lab1.py
@@ -13,20 +13,20 @@
         check_rules - function that checks rules conflicts
         check_rules_vs_facts - function that checks facts
         main - main function
-    """
+"""
 from random import choice, shuffle, randint
 from time import time
 
 
 def generate_simple_rules(code_max, n_max, n_generate, log_oper_choice=["and", "or", "not"]):
     """ Function generate simple rules
-            Args :
-                    code_max - max value
-                    n_max - max number of elements in condition
-                    log_oper_choice - operation in rule
-            Returns :
-                    rules - generating rules
-        """
+                Args :
+                        code_max - max value
+                        n_max - max number of elements in condition
+                        log_oper_choice - operation in rule
+                Returns :
+                        rules - generating rules
+    """
     rules = []
     for j in range(0, n_generate):
         log_oper = choice(log_oper_choice)  # not means and-not (neither)
@@ -48,12 +48,15 @@ def generate_simple_rules(code_max, n_max, n_generate, log_oper_choice=["and", "
 
 
 def generate_stairway_rules(code_max, n_max, n_generate, log_oper_choice=["and", "or", "not"]):
-    """ Function generate simple rules
+    """ Function generate stairway rules
                 Args :
                         code_max - max value
                         n_max - max number of elements
+                        n_generate - generate number of elements
                         log_oper_choice - operation in rule
-            """
+                Returns :
+                        rules - generating rules
+    """
     rules = []
     for j in range(0, n_generate):
         log_oper = choice(log_oper_choice)  # not means and-not (neither)
@@ -75,6 +78,15 @@ def generate_stairway_rules(code_max, n_max, n_generate, log_oper_choice=["and",
 
 
 def generate_ring_rules(code_max, n_max, n_generate, log_oper_choice=["and", "or", "not"]):
+    """ Function generate ring rules
+                Args :
+                        code_max - max value
+                        n_max - max number of elements
+                        n_generate - generate number of elements
+                        log_oper_choice - operation in rule
+                Returns :
+                        rules - generating rules
+    """
     rules = generate_stairway_rules(code_max, n_max, n_generate - 1, log_oper_choice)
     log_oper = choice(log_oper_choice)  # not means and-not (neither)
     if n_max < 2:
@@ -95,6 +107,15 @@ def generate_ring_rules(code_max, n_max, n_generate, log_oper_choice=["and", "or
 
 
 def generate_random_rules(code_max, n_max, n_generate, log_oper_choice=["and", "or", "not"]):
+    """ Function generate random rules
+                Args :
+                        code_max - max value
+                        n_max - max number of elements
+                        n_generate - generate number of elements
+                        og_oper_choice - operation in rule
+                Returns :
+                       rules - generating rules
+    """
     rules = []
     for j in range(0, n_generate):
         log_oper = choice(log_oper_choice)  # not means and-not (neither)
@@ -116,12 +137,25 @@ def generate_random_rules(code_max, n_max, n_generate, log_oper_choice=["and", "
 
 
 def generate_seq_facts(number_facts):
+    """ Function generate seq facts
+                Args :
+                        number_facts - numbers of facts
+                Returns :
+                        facts - generating facts
+    """
     facts = list(range(0, number_facts))
     shuffle(facts)
     return facts
 
 
 def generate_rand_facts(code_max, number_facts):
+    """ Function generate random facts
+                Args :
+                        code_max - max value of fact
+                        number_facts - numbers of facts
+                Returns :
+                        facts - generating facts
+    """
     facts = []
     for i in range(0, number_facts):
         facts.append(randint(0, code_max))
@@ -130,6 +164,12 @@ def generate_rand_facts(code_max, number_facts):
 
 # not final version
 def set_rang(rules):  # set rangs for rules
+    """ Function set rangs for rules
+                Args :
+                        rules - our rules for which set rangs
+                Returns :
+                        correct_rules - rules which have rang
+    """
     rangs = [-1] * 2 * len(rules)
     then_rules = list()
     max_rang = 0
@@ -158,6 +198,13 @@ def set_rang(rules):  # set rangs for rules
 
 
 def max_rangs(rule, rangs):  # calculate max rang of rule
+    """ Function determine max rang of rule
+                Args :
+                        rule - our rules to determine max rang
+                        rangs - list rangs of rules
+                Returns :
+                        max_rang - max rang of rule
+    """
     max_rang_if = 0
     for keys in rule['if']:
         for value in rule['if'][keys]:
@@ -174,6 +221,11 @@ def max_rangs(rule, rangs):  # calculate max rang of rule
 
 
 def sort_rangs(rules, rangs):  # sort rules by rang
+    """ Function sort rules by rangs
+                Args :
+                        rules - our rules
+                        rangs - list rangs of rules
+    """
     for i in range(len(rules) - 1):
         for j in range(len(rules) - i - 1):
             if max_rangs(rules[j], rangs) > max_rangs(rules[j+1], rangs):
@@ -186,6 +238,12 @@ def sort_rangs(rules, rangs):  # sort rules by rang
 
 
 def check_rules(rules):
+    """ Function check rules conflicts
+                Args :
+                        rules - our rules
+                Returns :
+                        correct_rules - rules without conflicts
+    """
     start_check = time()
     if_rules = list()
     then_rules = list()
@@ -231,6 +289,11 @@ def check_rules(rules):
 
 
 def check_rules_vs_facts(rules, facts):
+    """ Function check rules conflicts
+                Args :
+                        rules - our rules
+                        facts - our generating facts
+    """
     start_check = time()
     result = list()
     temp = 0
@@ -272,6 +335,9 @@ def check_rules_vs_facts(rules, facts):
 
 
 def main():
+    """
+        main function for work
+    """
     # generate rules
     time_start = time()
     number_rules = 10000
